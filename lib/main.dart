ac0d752ddbc9f67df421b99ee77b0bf0cf6eb9e0
Danny <93397389+Dcastro99@users.noreply.github.com>
2023-01-25 18:37:50 -0800

    added state

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,68 @@ void main() {
   ));
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
 
-  List<String> grocieries = ['Apples', 'Bananas', 'Oranges'];
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> groceries = ['Apples', 'Bananas', 'Oranges'];
   List<double> prices = [1.99, 2.99, 3.99];
+  final textController = TextEditingController();
+  int? groceryIndex;
+  IconData buttonIcon = Icons.add;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('This is the AppBar!')),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Expanded(
+                child: TextField(
+              controller: textController,
+            )),
+            ElevatedButton(
+              child: Icon(buttonIcon),
+              onPressed: () {
+                setState(() {
+                  if (groceryIndex != null) {
+                    groceries[groceryIndex!] = textController.text;
+                    groceryIndex = null;
+                    buttonIcon = Icons.add;
+                  } else {
+                    groceries.add(textController.text);
+                  }
+                  textController.clear();
+                });
+                print(groceries);
+              },
+            )
+          ],
+        ),
+      ),
       body: ListView.builder(
-        itemCount: grocieries.length,
+        itemCount: groceries.length,
         itemBuilder: (BuildContext context, int index) {
           return Card(
               child: ListTile(
-            title: Text(grocieries[index]),
-            trailing: Text('\$${prices[index]}'),
+            title: Text(groceries[index]),
+            trailing: const Text('\$-'),
+            onTap: () {
+              groceryIndex = index;
+              setState(() {
+                textController.text = groceries[index];
+                buttonIcon = Icons.save;
+              });
+            },
+            onLongPress: () {
+              setState(() {
+                groceries.removeAt(index);
+              });
+            },
           ));
         },
       ),
